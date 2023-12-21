f033ecfebabc596e60c56037e197d60ea782e63e
khaudayne <138597386+khaudayne@users.noreply.github.com>
2023-12-21 19:47:41 +0700

    Add files via upload

---
+++ repository/HumanRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Human;
+@Repository
+public interface HumanRepository extends JpaRepository<Human, Integer>{
+	@Query(value = "select * from human", nativeQuery = true)
+	List<Human> findAll();
+}
