ad6482174608ae7b018b2f2bda7f90b256d620f9
khaudayne <138597386+khaudayne@users.noreply.github.com>
2023-12-21 19:47:12 +0700

    Add files via upload

---
+++ entities/Human.java
@@ -0,0 +1,82 @@
+package com.example.demo.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "human")
+public class Human {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "dob")
+	private Date dob;
+	
+	@Column(name = "diachi")
+	private String diachi;
+	
+	public Human() {
+		
+	}
+	public Human(String name, String gender, Date dob, String diachi) {
+		this.name = name;
+		this.gender = gender;
+		this.dob = dob;
+		this.diachi = diachi;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getDiachi() {
+		return diachi;
+	}
+
+	public void setDiachi(String diachi) {
+		this.diachi = diachi;
+	}
+	
+	
+}
